e5b64d9220bf1b582ff4f2947fd23ad18c8a27c0
Supriya-M810 <supriya.mathiyalagan@cognizant.com>
2024-06-29 20:54:46 +0530

    Update streamlit_app.py
    added snowflake.snowpark.context import session instead of snowflake.snowpark.context import cnx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import cnx
+from snowflake.snowpark.context import session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
